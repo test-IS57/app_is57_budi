a5451889a85ac1722b29d1bf56ed8c06fe3937bd
test-IS57 <is57lp3i@gmail.com>
2022-07-06 10:59:24 +0700

    membuat migration

--- database/migrations/2022_07_06_035202_create_karyawans_table.php
+++ database/migrations/2022_07_06_035202_create_karyawans_table.php
@@ -13,14 +13,16 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('karyawans', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->enum('hak_akses', ['admin', 'user']);
-            $table->rememberToken();
+            $table->string('nik')->unique();
+            $table->string('nama');
+            $table->string('tempat lahir');
+            $table->date('tanggal_lahir');
+            $table->string('email');
+            $table->bigInteger('jabatans_id');
+            $table->string('alamat');
+            $table->string('pendidikan_terakhir');
             $table->timestamps();
         });
     }
@@ -32,6 +34,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('karyawans');
     }
 };
